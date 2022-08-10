5aff79498fc2ca258a6227bb7b2f4810116cfd72
lordCman <crodriguez1086@yahoo.com>
2022-08-10 18:23:56 -0400

    new

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from app import app
-from app.models import User, Post, db
+from app.models import User, Post, db, Shopitems
+from app.shop.forms import CreateShopItemForm
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post, 'Shopitems': Shopitems}
 
 if __name__ == '__main__':
     app.run()
